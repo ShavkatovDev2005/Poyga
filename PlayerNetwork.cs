762baf403268d71ae2ec235103f3df3ba82f445c
ShavkatovDev2005 <devshavkatov@gmail.com>
2025-06-13 22:08:50 +0500

    _

--- PlayerNetwork.cs
+++ PlayerNetwork.cs
@@ -1,5 +1,8 @@
+using SimpleInputNamespace;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerNetwork : NetworkBehaviour
 {
@@ -12,7 +15,7 @@ public class PlayerNetwork : NetworkBehaviour
         },
         NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner
     );
-        
+
     public struct MyCustomData : INetworkSerializable
     {
         public int _int;
@@ -33,48 +36,164 @@ public class PlayerNetwork : NetworkBehaviour
             Debug.Log(OwnerClientId + " ; " + newValue._int + " ; " + newValue._bool + " ; " + newValue._string);
         };
     }
-    void Update()
-    {
-        if (!IsOwner) return;
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ServerRPC();
-            // randomNumber.Value = new MyCustomData
-            // {
-            //     _int = Random.Range(0, 100),
-            //     _bool = false,
-            //     _string = "Hello from " + OwnerClientId
-            // };
-        }
-
-        Vector3 moveDir = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            moveDir += Vector3.forward;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            moveDir += Vector3.back;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveDir += Vector3.left;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            moveDir += Vector3.right;
-        }
-
-        if (moveDir != Vector3.zero)
-        {
-            transform.position += moveDir * 10 * Time.deltaTime;
-        }
-    }
 
     [ServerRpc]
     private void ServerRPC()
     {
         Debug.Log("ServerRPC called by client: " + OwnerClientId);
     }
+    public InputActionReference move;
+    public SteeringWheel gas;
+    public AxisInputUIArrows rul;
+    public Rigidbody rb;
+    public wheelControllers colliders;
+    public wheelMeshes meshes;
+    public float breakPower;
+    public float motorpower;
+    private float slipAngle;
+    public float speed;
+    public Camera _camera;
+    Vector2 inputVector;
+    public float gasInsput,steeringInput,breakeInput;
+    public GameObject keyboard,mobile;
+    public GameObject mobileUI;
+    bool UseKeyboard;
+    void Start()
+    {
+        if (!IsOwner)
+        {
+            _camera.enabled = false;
+            _camera.gameObject.SetActive(false);
+        }
+        else
+        {
+            _camera.enabled = true;
+            _camera.gameObject.SetActive(true);
+        }
+        ChangeKeyboard();
+    }
+
+
+    void Update()
+    {
+        if (!IsOwner) return;
+
+        speed = rb.linearVelocity.magnitude;
+        UpdateWheels();
+        checkInput();
+        ApplyMotor();
+        ApplySteering();
+        applyBreak();
+
+        inputVector = move.action.ReadValue<Vector2>();
+
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     ServerRPC();
+        //     // randomNumber.Value = new MyCustomData
+        //     // {
+        //     //     _int = Random.Range(0, 100),
+        //     //     _bool = false,
+        //     //     _string = "Hello from " + OwnerClientId
+        //     // };
+        // }
+    }
+
+    void ApplySteering()
+    {
+        float angle = steeringInput * 45;
+        colliders.FRwheel.steerAngle = angle;
+        colliders.FLwheel.steerAngle = angle;
+    }
+    void ApplyMotor()
+    {
+        colliders.RRwheel.motorTorque = motorpower * -gasInsput;
+        colliders.RLwheel.motorTorque = motorpower * -gasInsput;
+    }
+    public void ChangeKeyboard()
+    {
+        if (!IsOwner) return;
+        UseKeyboard = !UseKeyboard;
+        mobile.SetActive(!UseKeyboard);
+        keyboard.SetActive(UseKeyboard);
+        if (UseKeyboard)
+        {
+            mobileUI.SetActive(false);
+            move.action.Enable();
+        }
+        else
+        {
+            mobileUI.SetActive(true);
+            move.action.Disable();
+        }
+    }
+    void checkInput()
+    {
+        if (UseKeyboard)
+        {
+            gasInsput = inputVector.y;
+            steeringInput = inputVector.x;
+        }
+        else
+        {
+            gasInsput = rul.Value.x;
+            steeringInput = gas.Value;
+        }
+
+        slipAngle = Vector3.Angle(transform.forward, rb.linearVelocity-transform.forward);
+        if (slipAngle < 120)
+        {
+            if (gasInsput < 0)
+            {
+                breakeInput = Mathf.Abs(gasInsput);
+                gasInsput = 0;
+            }
+        }
+        else{
+            breakeInput = 0;
+        }
+    }
+
+
+    void applyBreak()
+    {
+        colliders.FRwheel.brakeTorque = breakPower * breakeInput * 0.7f;
+        colliders.FLwheel.brakeTorque = breakPower * breakeInput * 0.7f;
+        colliders.RRwheel.brakeTorque = breakPower * breakeInput * 0.3f;
+        colliders.RLwheel.brakeTorque = breakPower * breakeInput * 0.3f;
+    }
+    void UpdateWheels()
+    {
+        UpdateWheel(colliders.FRwheel, meshes.FRwheel);
+        UpdateWheel(colliders.FLwheel, meshes.FLwheel);
+        UpdateWheel(colliders.RRwheel, meshes.RRwheel);
+        UpdateWheel(colliders.RLwheel, meshes.RLwheel);
+    }
+    void UpdateWheel(WheelCollider collider, GameObject mesh)
+    {
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+        mesh.transform.position = position;
+        mesh.transform.rotation = rotation;
+    }
+}
+
+
+//------------------------------------------------------------------------------------------------------------
+[System.Serializable]
+public class wheelControllers
+{
+    public WheelCollider FRwheel;
+    public WheelCollider FLwheel;
+    public WheelCollider RRwheel;
+    public WheelCollider RLwheel;
+}
+[System.Serializable]
+public class wheelMeshes
+{
+    public GameObject FRwheel;
+    public GameObject FLwheel;
+    public GameObject RRwheel;
+    public GameObject RLwheel;
 }
